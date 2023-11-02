9272953fed3f96c4255ea66b933c55819ed9f22f
stalemoon <thanawan477@gmail.com>
2023-11-03 03:19:55 +0700

    add scrollable welcome for big element (android)

--- Welcome.js
+++ Welcome.js
@@ -1,4 +1,4 @@
-import { View, Text, Pressable, Image, Dimensions, StatusBar } from 'react-native'
+import { View, Text, Pressable, Image, Dimensions, StatusBar, ScrollView, Platform } from 'react-native'
 import React from 'react'
 import { LinearGradient } from "expo-linear-gradient";
 import COLORS from './fifa/colors';
@@ -17,72 +17,76 @@ const Welcome = ({ navigation }) => {
         <LinearGradient
             style={{
                 flex: 1
-
+                
             }}
             colors={[
                 COLORS.white,
                 COLORS.white
             ]}>
-            <View style={{
-                padding: 10,
-                flex: 1
-            }}>
-                {/* <StatusBar hidden /> */}
-                <View style={[{ padding: 24, marginTop: 24, }]}>
-                    <Image
-                        source={require("./picture/LogoApp.png")}
-                        style={{
-                            height: 138,
-                            width: 244,
-                        }}
-                    />
-                    <Text style={{
-                        paddingTop: 20,
-                        fontSize: 30,
-                        fontWeight: 600,
-                        color: COLORS.black
-                    }}>Welcome to</Text>
-                    <Text style={{
-                        fontSize: 27,
-                        fontWeight: 600,
-                        color: COLORS.black
-                    }}>KMUTT LiB</Text>
+            <ScrollView
+                contentContainerStyle={[{ flexGrow: 1 }]}
+                showsVerticalScrollIndicator={false}>
+                <View style={{
+                    padding: 10,
+                    flex: 1
+                }}>
+                    {/* <StatusBar hidden /> */}
 
-                </View>
-                <View style={[{ padding: 12, alignItems: 'center', }]}>
-                    <Image
-                        source={require("./picture/iconWelcome.png")}
-                        style={{
-                            height: 371,
-                            width: 354,
-                        }}
-                    />
-                </View>
-                <View style={[{ alignItems: 'center', }]}>
-                    <Button
-                        title="Sign in with Email"
-                        onPress={() => navigation.navigate("LoginFIFA")}
-                        style={{
-                            marginTop: 22,
-                            borderColor: COLORS.primary,
-                            width: "90%",
-                            color: COLORS.black,
-                        }}
-                    />
-                    <Button
-                        title="Login with guest"
-                        onPress={() => navigation.navigate("MainNavigator")}
-                        style={{
-                            marginTop: 22,
-                            borderColor: COLORS.grey,
-                            backgroundColor: COLORS.grey,
-                            // textColor: 
-                            width: "90%",
+                    <View style={[{ padding: 24, marginTop: 24, }]}>
+                        <Image
+                            source={require("./picture/LogoApp.png")}
+                            style={{
+                                height: 138,
+                                width: 244,
+                            }}
+                        />
+                        <Text style={{
+                            paddingTop: 10,
+                            fontSize: 30,
+                            fontWeight: 600,
                             color: COLORS.black
-                        }}
-                    />
+                        }}>Welcome to</Text>
+                        <Text style={{
+                            fontSize: 27,
+                            fontWeight: 600,
+                            color: COLORS.black
+                        }}>KMUTT LiB</Text>
+
+                    </View>
+                    <View style={[{ alignItems: 'center', }]}>
+                        <Image
+                            source={require("./picture/iconWelcome.png")}
+                            style={{
+                                height: 371,
+                                width: 354,
+                            }}
+                        />
+                    </View>
+                    <View style={[{ alignItems: 'center', }]}>
+                        <Button
+                            title="Sign in with Email"
+                            onPress={() => navigation.navigate("LoginFIFA")}
+                            style={{
+                                borderColor: COLORS.primary,
+                                width: "90%",
+                                color: COLORS.black,
+                            }}
+                        />
+                        <Button
+                            title="Login with guest"
+                            onPress={() => navigation.navigate("MainNavigator")}
+                            style={{
+                                marginTop: 10,
+                                borderColor: COLORS.grey,
+                                backgroundColor: COLORS.grey,
+                                // textColor: 
+                                width: "90%",
+                                color: COLORS.black
+                            }}
+                        />
+                    </View>
                 </View>
-            </View>
+            </ScrollView>
         </LinearGradient >
     )
 }
