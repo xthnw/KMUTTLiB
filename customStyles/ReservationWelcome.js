cca766bc702d0e996a18aba183d4a60818ea99ce
Tanaton <fifa52766@gmail.com>
2023-11-16 17:15:16 +0700

    add style

--- customStyles/ReservationWelcome.js
+++ customStyles/ReservationWelcome.js
@@ -5,19 +5,93 @@ const screenHeight = Dimensions.get('window').height;
 const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor:COLORS.primary,
+  container:{
+    flex: 1, 
+    backgroundColor: COLORS.white 
+  },
+  paddingSpace:{
+    padding: 10,
+    flex: 1
+  },
+  paddingSpace2:{
+    padding: 24, marginTop: 24,
+  },
+  imgSize:{
+    height: 128,
+    width: 128,
+  },
+
+    contentContainer: {
+      alignItems: 'center',
+       flex: 1 
+    },
+    title: {
+      paddingTop: 10,
+      fontSize: 40,
+      fontFamily: 'LeagueSpartanSemiBold',
+      color: COLORS.black
+    },
+    subtitle: {
+      fontSize: 37,
+      fontFamily: 'LeagueSpartanSemiBold',
+      color: COLORS.black
+    },
+
+    imgSize2: {
+
+        height: 371,
+        width: 354,
+    },
+    input:{
+      fontFamily: 'LeagueSpartan',
+      width: '100%'
+    },
+    button: {
+      fontFamily: 'LeagueSpartan',
+      width:screenWidth*0.8,
+      alignItems: 'center',
+      marginTop: 10,
+      borderColor: COLORS.primary,
+      borderWidth: 2,
+      borderRadius: 12,
+      backgroundColor: COLORS.primary,
+      paddingBottom: 16,
+      paddingVertical: 10,
+
+    },
+    toggledBox: {
+      borderColor: COLORS.error,
+
+    },
+  TextError: {
+    height: screenHeight*0.05,
+    color: COLORS.error,
+  },
+  eyePosition:{
+    position: 'absolute',
+    right: 12
+
+  },
+  textButton:{
     fontFamily: 'LeagueSpartan',
-    paddingBottom: 16,
-    paddingVertical: 10,
-    borderColor: COLORS.primary,
-    borderWidth: 2,
-    borderRadius: 12,
-    alignItems: 'center',
-    width:screenWidth*0.8,
-   
-},
-    buttonGust:{     
+    fontSize: 18,
+    color:COLORS.white 
+  },
+
+  positionTest:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 22
+  },
+  titleTest:{
+    color:COLORS.secondary,
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    
+    fontFamily: 'LeagueSpartan'
+  },
+  buttonGust:{     
     fontFamily: 'LeagueSpartan',
     width:screenWidth*0.8,
     alignItems: 'center',
@@ -31,54 +105,6 @@ const styles = StyleSheet.create({
     color: COLORS.black
     },
 
-    contentContainer: {
-      backgroundColor: '#fbfbfb',
-      borderRadius: 10,
-      padding: 20,
-      elevation: 3,
-    },
-    title: {
-      fontSize: 22,
-      fontWeight: 'bold',
-      marginVertical: 10,
-      color: COLORS.black,
-      fontFamily: 'LeagueSpartan'
-    },
-    subtitle: {
-      fontSize: 16,
-      color: COLORS.black,
-      fontFamily: 'LeagueSpartan',
-    },
-
-    titleInput: {
-      fontSize: 16,
-      fontWeight: 400,
-      marginVertical: 8,
-      fontFamily: 'LeagueSpartan',
-    },
-    Input: {
-      width: '100%',
-      height: 48,
-      borderColor: COLORS.black,
-      borderWidth: 1,
-      borderRadius: 8,
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingLeft: 22,
-      fontFamily: 'LeagueSpartan',
-    },
-    error: {
-      width: '100%',
-      height: 48,
-      borderColor: COLORS.error,
-      borderWidth: 1,
-      borderRadius: 8,
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingLeft: 22,
-      fontFamily: 'LeagueSpartan',
-    },
-    
   });
 
   export default styles;
